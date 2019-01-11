0654e54a4eb4e92b252aaf49f7479f166e39d514
Algoud Marek <marek.algoud@gmail.com>
2019-01-11 13:12:20 +0100

    update exo 2

--- src/App.js
+++ src/App.js
@@ -112,23 +112,9 @@ class App extends Component {
         <div className="intro">
           <h1>But de l'exercice :</h1>
           <ol>
-            <li>créer un composant Marque qui a comme propriété
-            <ul>
-                <li>Un Nom</li>
-                <li>Un lien vers une image</li>
-              </ul>
-            </li>
-            <li>
-              Créer un composant pneu qui a comme propriété
-            <ul>
-                <li>Un nom</li>
-                <li>Une marque (id de la marque)</li>
-                <li>Une description (avec du contenu HTML)</li>
-              </ul>
-            </li>
-            <li>Dans le fichier App.js créer un tableau contenant la liste des marque et un tableau content la liste des pneus, et les afficher</li>
-            <li>Rajouter un champ input qui permet de filtrer les pneus (sur le champ nom)</li>
-            <li>Supprimer une marque au clic sur celle-ci (si elle n'est pas utiliser par un pneu)</li>
+            <li>Masquer par défaut la liste des marques</li>
+            <li>Rajouter un bouton qui permet d'afficher / masquer la liste des marques</li>
+            <li>Transformer le composant marque en class, et lui rajouter les fonctions du cycle de vie, (avec un console.log), idem pour la class App</li>
           </ol>
         </div>
         <h2> Liste des marques</h2>
