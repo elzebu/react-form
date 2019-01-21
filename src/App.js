7d8ec76940a44539255002382b69904c78f95b08
Algoud Marek <marek.algoud@gmail.com>
2019-01-21 15:20:57 +0100

    add react loadable

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 import { fetchBrands } from './redux/actions';
 
+import Loadable from 'react-loadable';
+
 import Brands from './component/Brands/Brands';
 import Tyres from './component/Tyres/Tyres';
 import Home from './component/Home';
 import Header from './component/Header';
 import TyreDetail from './component/TyreDetail';
-import TyreEdit from './component/TyreEdit';
+
 import BrandEdit from './component/BrandEdit';
 
 import './App.css';
 
+const LoadableTyreEdit = Loadable({
+  loader: () => import('./component/TyreEdit', /* webpackChunkName: "tyreEdit" */),
+  loading: () => <h1>chargement...</h1>,
+});
+
 class App extends Component {
+
+
+  onMouseOver = () => {
+    LoadableTyreEdit.preload();
+  }
 
   render () {
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className="App" onMouseOver={this.onMouseOver}>
           <Helmet titleTemplate="%s | allopneu.com"></Helmet>
           <div className="intro">
             <ul>
@@ -31,7 +43,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/tyres" component={Tyres} />} />
             <Route exact path="/tyres/detail/:id" component={TyreDetail} />
-            <Route exact path="/tyres/detail/:id/edit" component={TyreEdit} />
+            <Route exact path="/tyres/detail/:id/edit" component={LoadableTyreEdit} />
             <Route exact path="/brands" component={Brands} />
             <Route exact path="/brands/:id/edit" component={BrandEdit} />
             <Redirect from="*" to="/" />
