91ee1982d48602ecc283ab891995525654708628
Algoud Marek <marek.algoud@gmail.com>
2019-01-22 10:29:59 +0100

    add css.module for header component

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
+import style from './header.module.css';
+
 const Header = (props) => {
     return (
-    <header>
+    <header className={style.header}>
         <Link to="/">Home</Link>
         <Link to="/tyres">Pneu</Link>
         <Link to="/brands">Marque</Link>
