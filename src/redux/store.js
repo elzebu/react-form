eb223928d9f7e61b7672af6d091315a1953877f2
Algoud Marek <marek.algoud@gmail.com>
2020-03-12 10:44:25 +0100

    exo 11 solved

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import * as reducers from './reducers'
 
 const initialState = {
     theme: 'white'
 }
 
-const store = createStore(reducers.theme, initialState)
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] dispaching', action)
+            const result = next(action)
+            console.log('[Middleware] next state', store.getState())
+            return result
+        }
+     }
+}
+
+const store = createStore(reducers.theme, initialState, applyMiddleware(logger))
 
 export default store
